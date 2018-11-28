5d7d7118c20e9b3ab8aa5624befcede206d9ed5d
ryanstgermain <ryanjoel26@gmail.com>
2018-11-28 10:47:46 -0700

    fixed an error in migrations, added a get by id route and post route

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ const port = process.env.PORT || 3000
 const knex = require('knex');
 const config = require("./knexfile");
 const queries = require('./queries.js');
+const cors = require('cors')
 
-//app.get("/", function(req, res) {
-    //res.send("boi");
-//})
+//app.use(cors())
 
 app.get("/", function(req, res) {
     queries.getAll().then(response => res.send(response))
+})
+
+app.get('/:id', function(req, res) {
+    queries.getById(req.params.id).then(students => res.send(students))
+})
+
+app.post('/', (req, res) => {
+    queries.createStudent(req.body).then(students => res.send(students))
 })
 
 app.get("*", function(req, res) {
--- queries.js
+++ queries.js
@@ -3,5 +3,11 @@ const db = require('./database-connections.js')
 module.exports = {
     getAll() {
        return db.select().from('students') 
+    },
+    getById(id){
+        return db('students').where('id', id)
+    },
+    createStudent(newStudent){
+        return db('students').insert(newStudent)
     }
 }
